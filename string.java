56de6a93a7e711e696e86a7dcb370ada918d0190
Mahamood <shaikmisba305@gmail.com>
2024-12-11 13:32:00 +0530

    Add files via upload

---
+++ string.java
@@ -0,0 +1,11 @@
+class Main{
+    public static String s()
+    {
+        return "Hello Mahamood";
+    }
+    public static void main(String args[])
+    {
+        String res=s();
+        System.out.println(res);
+    }
+}
